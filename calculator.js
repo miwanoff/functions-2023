a803ecc85d06c3c85ca74d15d6fd10ecd99feda3
miwanoff <marina.iva.noffa@gmail.com>
2023-10-07 13:14:37 +0300

    Add minus function

--- calculator.js
+++ calculator.js
@@ -5,6 +5,18 @@ let action = "";
 let result = 0;
 
 function add() {
-    val1 = document.getElementById("num").value;
-    alert(val1);
+  val1 = +document.getElementById("num").value;
+  action = "+";
+  //alert(val1 + action);
+}
+
+function minus() {
+    val1 = +document.getElementById("num").value;
+    action = "-";
+    //alert(val1 + action);
+  }
+
+function calc() {
+  val2 = +document.getElementById("num").value;
+  alert(val1 + action + val2);
 }
