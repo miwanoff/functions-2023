5cb7dd40e0066e5753c1f4fc3695b99d578ec68d
miwanoff <marina.iva.noffa@gmail.com>
2023-10-07 12:23:12 +0300

    Add trafficLight2

--- traffic-lights.js
+++ traffic-lights.js
@@ -3,11 +3,17 @@
 function trafficLight() {
     let color = "green";
     let light = function () {
+        color =  (color == "green")? "red" : "green";
         return color;
     };
     return light;
 } 
 
-let trafficLight1 = trafficLight(); // первый счетчик
+let trafficLight1 = trafficLight(); // 1
 console.log(trafficLight1()); 
 console.log(trafficLight1()); 
+
+let trafficLight2 = trafficLight(); // 2
+console.log(trafficLight2()); 
+console.log(trafficLight1()); 
+console.log(trafficLight2()); 
