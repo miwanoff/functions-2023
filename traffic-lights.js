9a923efcfc5aec2153580ab292b508ba22a3c7a9
miwanoff <marina.iva.noffa@gmail.com>
2023-10-07 12:47:53 +0300

    Add name

--- traffic-lights.js
+++ traffic-lights.js
@@ -1,24 +1,27 @@
 "use strict";
 
-function trafficLight() {
+function trafficLight(name) {
     let color = "green";
     let light = function () {
         color =  (color == "green")? "red" : "green";
-        return color;
+        let arr = {};
+        arr.name = name;
+        arr.color = color;
+        return arr;
     };
     return light;
 } 
 
-let trafficLight1 = trafficLight(); // 1
+let trafficLight1 = trafficLight("One"); // 1
 console.log(trafficLight1()); 
 console.log(trafficLight1()); 
 
-let trafficLight2 = trafficLight(); // 2
+let trafficLight2 = trafficLight("two"); // 2
 console.log(trafficLight2()); 
 console.log(trafficLight1()); 
 console.log(trafficLight2()); 
 
 let c = trafficLight1();
-console.log(`%c ${c}`, `background: ${c}; color: #bada55`);
-c = trafficLight1();
-console.log(`%c ${c}`, `background: ${c}; color: #bada55`);
+console.log(`%c ${c.color}`, `background: ${c.color}; color: #bada55`);
+// c = trafficLight1();
+// console.log(`%c ${c}`, `background: ${c}; color: #bada55`);
